bd39503ae585a096f2bf393fcfc68276511bf2c9
nelsonmakori <nelsonmakori6@gmail.com>
2024-07-10 16:29:15 -0600

    generateHTML

--- chickfla.js
+++ chickfla.js
@@ -1,22 +1,49 @@
+
+let menu = document.getElementById('menu')
+let orderList = document.getElementById('orderList')
+
+menu.addEventListener('mouseover',()=>{
+  orderList.style.display = 'block'
+  console.log('hapo vipi')
+})
+menu.addEventListener('mouseout',()=>{
+  orderList.style.display = 'none'
+  console.log('hapo vipi')
+})
+console.log(orderList)
+
+
 const products = [
   {
-    image: "chikfla.images/Grilled_sandwich.png",
-    price: 8.79,
-    descripition: '480 cal per sandwich',
+    id:"3",
+    image:"chikfla.images/salads.png",
+    price: 7.99,
+    name:"salad"
   },
   {
-    image: "chikfla.images/meals.png",
-    price: "10.49",
-    descripition:"medium coca-cola cherry"
+    id:"1",
+    image:'chikfla.images/treats.png',
+    price: 100,
+    name:"limited time flavors"
   },
   {
-    image: "chikfla.images/8oz sauces.png",
-    price: 3.00,
-    descripition:"8oz chick-fil-A "
-  }
-  
+    id:"2",
+    image:"chikfla.images/Beverages.png",
+    price: 10.99,
+    name:"beverage"
+  } 
 ]
+let generateHTML = ""
 
-
+products.forEach((menu)=>{
+ generateHTML += `<div class="meals">
+ <img src="${menu.image}">
+ <div>${menu.name}</div>
+  $${menu.price}
+  </div>
+`
+});
+console.log(generateHTML)
+ document.querySelector('.products').innerHTML = generateHTML
 
 
