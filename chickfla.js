3364ba24a46d016a5db20c7ba8b3bd311179fb1e
nelsonmakori <nelsonmakori6@gmail.com>
2024-07-15 12:58:32 -0600

    added Data attribute
    made cart interactive

--- chickfla.js
+++ chickfla.js
@@ -1,49 +1,60 @@
+let menu = document.getElementById("menu");
+let orderList = document.getElementById("orderList");
 
-let menu = document.getElementById('menu')
-let orderList = document.getElementById('orderList')
+menu.addEventListener("mouseover", () => {
+  orderList.style.display = "block";
+  console.log("hapo vipi");
+});
+menu.addEventListener("mouseout", () => {
+  orderList.style.display = "none";
+});
+console.log(orderList);
 
-menu.addEventListener('mouseover',()=>{
-  orderList.style.display = 'block'
-  console.log('hapo vipi')
-})
-menu.addEventListener('mouseout',()=>{
-  orderList.style.display = 'none'
-  console.log('hapo vipi')
-})
-console.log(orderList)
+let generateHTML = "";
 
-
-const products = [
-  {
-    id:"3",
-    image:"chikfla.images/salads.png",
-    price: 7.99,
-    name:"salad"
-  },
-  {
-    id:"1",
-    image:'chikfla.images/treats.png',
-    price: 100,
-    name:"limited time flavors"
-  },
-  {
-    id:"2",
-    image:"chikfla.images/Beverages.png",
-    price: 10.99,
-    name:"beverage"
-  } 
-]
-let generateHTML = ""
-
-products.forEach((menu)=>{
- generateHTML += `<div class="meals">
+products.forEach((menu) => {
+  generateHTML += `<div class="meals">
  <img src="${menu.image}">
  <div>${menu.name}</div>
   $${menu.price}
+  <br>
+  <button class="add-cart" data-food-name = "${menu.name}">add to cart</button>
   </div>
-`
+`;
 });
-console.log(generateHTML)
- document.querySelector('.products').innerHTML = generateHTML
+console.log(generateHTML);
+document.querySelector(".products").innerHTML = generateHTML;
 
+const addToCart = document.querySelectorAll(".add-cart");
+addToCart.forEach((button) => {
+  button.addEventListener("click", () => {
+    const mealName = button.dataset.foodName;
 
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (mealName === item.mealName) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        mealName: mealName,
+        quantity: 1,
+      });
+      
+    }
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    const totalCart = document.querySelector(".cart-quantity");
+    totalCart.innerHTML = cartQuantity;
+  });
+});
---
+++ products.js
@@ -0,0 +1,21 @@
+
+const products = [
+  {
+    id:"3",
+    image:"chikfla.images/salads.png",
+    price: 7.99,
+    name:"salad"
+  },
+  {
+    id:"1",
+    image:'chikfla.images/treats.png',
+    price: 100,
+    name:"limited time flavors"
+  },
+  {
+    id:"2",
+    image:"chikfla.images/Beverages.png",
+    price: 10.99,
+    name:"beverage"
+  } 
+]
