13824c77b5159f2982f4570c7259ba15b02aa0d1
nelsonmakori <nelsonmakori6@gmail.com>
2024-08-09 12:35:53 -0600

    local storage
    saving local storage

--- cart.js
+++ cart.js
@@ -1,47 +1,54 @@
- 
- export let cart = [
-  {
-  mealId: "3",
-  quantity: 1,
- },{
-  mealId: "2",
-  quantity: 2,
-},{
-  mealId: "1",
-  quantity: 3,
-}
-]
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-  export function addCart(mealId){
-  
+if (!cart) {
+  cart = [
+    {
+      mealId: "3",
+      quantity: 1,
+    },
+    {
+      mealId: "2",
+      quantity: 2,
+    },
+    {
+      mealId: "1",
+      quantity: 3,
+    },
+  ];
+}
+
+function saveStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addCart(mealId) {
   let matchingItem;
 
-    cart.forEach((cartItem) => {
-      if (mealId === cartItem.mealId) {
-        matchingItem = cartItem;
-      }
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        mealId: mealId,
-        quantity: 1,
-      })
-    };
-}
- export function removeFromCart(mealId){
-  const newCart = []
-  cart.forEach((cartItem)=>{
-    if(cartItem.mealId !== mealId){
-      newCart.push(cartItem)
+  cart.forEach((cartItem) => {
+    if (mealId === cartItem.mealId) {
+      matchingItem = cartItem;
     }
   });
-  cart = newCart;
 
-
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      mealId: mealId,
+      quantity: 1
+    });
+  }
+  saveStorage();
 }
 
+export function removeFromCart(mealId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.mealId !== mealId) {
+      newCart.push(cartItem);
+    }
+  });
 
-
+  cart = newCart;
+  saveStorage();
+}
